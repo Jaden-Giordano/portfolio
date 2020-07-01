bdf38622cc886102d4c6fd82cc9a70ec2e6d1de6
Jaden Giordano <thejadenjack@gmail.com>
2020-06-30 19:58:29 -0700

    use wasm-bindgen

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import rustPlayground from '../webgl.wasm';
-
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ webgl/src/lib.rs
@@ -0,0 +1,13 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn update() {
+
+}
+
+#[wasm_bindgen]
+pub fn render() {
+
+}
