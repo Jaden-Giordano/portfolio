d658cb0910c0d4e9e689df8dd3cebe0d708adb83
Jaden Giordano <thejadenjack@gmail.com>
2020-09-03 18:11:00 -0700

    dockerization :D

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   lintOnSave: false,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'greymouth.io';
+      return args;
+    });
+  },
 };
