d1b53909ab56721aba8e90e5f8c1e2b3c403c744
Jaden Giordano <thejadenjack@gmail.com>
2021-03-07 16:48:34 -0800

    add my config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,66 +1,67 @@
 export default {
-  name: "Aidan Chelig",
-  tags: ["Frontend", "Game Development", "Sys Admin"],
-  copyrightText: "2021 &copy; Aidan Chelig",
-  projectsTitle: "What I've been working on...",
+  name: 'Jaden Giordano',
+  tags: ['Frontend', 'Game development', 'Design'],
+  copyrightText: '2021 &copy; Jaden Giordano',
+  projectsTitle: 'What I\'ve been working on...',
   projects: [
     {
-      name: "SSHero",
-      language: "Node.js",
-      description:
-        "A MUD like game written in javascript that uses a secure shell as the client.",
+      name: 'Deno Crypto',
+      language: 'deno',
+      description: 'An experimentation in creating a blockchain using the ED25519 encryption scheme.',
       links: [
         {
-          name: "Souce code",
-          href: "https://github.com/Aidan-Chelig/SSHero",
+          name: 'Souce code',
+          href: 'https://github.com/Jaden-Giordano/deno-crypto'
         },
       ],
     },
     {
-      name: "Portfolio Algorithmic Art",
-      language: "rust",
-      description:
-        "A friend of mine and I love cellular automata as well as generative art displays. We both have been experimenting in rust in the browser with WASM. Scroll up to view it!",
+      name: 'Portfolio Algorithmic Art',
+      language: 'rust',
+      description: 'A friend of mine and I love cellular automata as well as generative art displays. We both have been experimenting in rust in the browser with WASM. Scroll up to view it!',
       links: [
         {
-          name: "My site",
-          href: "http://aidanmadeit.xyz",
+          name: 'Aidan\'s site',
+          href: 'http://aidanmadeit.xyz',
         },
         {
-          name: "Source code",
-          href: "https://github.com/Aidan-Chelig/portfolio",
+          name: 'Source code',
+          href: 'https://github.com/Aidan-Chelig/portfolio',
         },
       ],
     },
     {
-      name: "Voice.rs",
-      language: "rust",
-      description: "An experimentation with Rust netcode and opus voice codec.",
+      name: 'Rogue Pets',
+      language: 'godot',
+      description: 'One weekend, four people, and one game. Three others and I teamed up for Ludum Dare 41 to create a game in a weekend. It was tons of fun and my first time creating a game in the Godot engine.',
       links: [
         {
-          name: "Source Code",
-          href: "https://github.com/Aidan-Chelig/voicers",
+          name: 'Itch.io Page',
+          href: 'https://lodugh.itch.io/roguepets',
+        },
+        {
+          name: 'Source Code',
+          href: 'https://github.com/Jaden-Giordano/LD41-SymbolicOrigin',
         },
       ],
     },
   ],
   externalLinks: [
     {
-      name: "Github",
-      href: "https://github.com/Aidan-Chelig",
+      name: 'Github',
+      href: 'https://github.com/Jaden-Giordano',
     },
     {
-      name: "Email",
-      href: "mailto:aidan.chelig@gmail.com",
+      name: 'Email',
+      href: 'mailto:thejadenjack@gmail.com',
     },
     {
-      name: "Linkedin",
-      href: "https://www.linkedin.com/in/aidan-chelig",
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/in/jaden-giordano-93947a17b/',
     },
     {
-      name: "Resume",
-      href:
-        "https://drive.google.com/file/d/1G3cGu572gyr7cMqneHjFX6rSq7tB9f-S/view?usp=sharing",
+      name: 'Resume',
+      href: 'https://drive.google.com/file/d/1g3edf3uLON720nxMxItI8b07zGmiPncS/view?usp=sharing',
     },
   ],
 };
